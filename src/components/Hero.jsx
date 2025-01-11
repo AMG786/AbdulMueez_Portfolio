0bd9234183505742423c9582daeb18addf696b0a
AMG786 <abdulmueezgujjar@gmail.com>
2025-01-11 16:25:39 +1100

    Hero section -text update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Abdul Mueez</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am  {/*<br className='sm:block hidden' />*/}
+            I am  a {/*<br className='sm:block hidden' />*/}
             Software Engineer
           </p>
         </div>
